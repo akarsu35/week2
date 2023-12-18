173851edafbf6073dc6edb6cab94eee19e2e86e6
akarsu35 <cumhurakarsu@gmail.com>
2023-12-18 15:55:34 +0300

    commit1

--- usHesaplama/src/Main.java
+++ usHesaplama/src/Main.java
@@ -7,14 +7,18 @@ public class Main {
         if(n==0){
             return 1;
         }else{
-            return a*pow(a,n-1);
+            return a*pow(a,n-1);//n=0 olana kadar fonksiyon
+                                    // tekrarlı olarak çağrılır.
+
         }
 
     }
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        //kullanıcıdan taban değerini alıyoruz.
         System.out.println("taban sayısını gir: ");
         int a=input.nextInt();
+        //kullanıcıdan üs değerini alıyoruz.
         System.out.println("üssü giriniz: ");
         int n=input.nextInt();
 
