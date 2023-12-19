08f340c9457083ce71ed626715ccc0f408a1fc52
akarsu35 <cumhurakarsu@gmail.com>
2023-12-19 12:07:42 +0300

    commit1

--- deseneGoreMetod/src/Main.java
+++ deseneGoreMetod/src/Main.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
+
 public class Main {
-    static int desen(int baslangic,int i){
 
-        if(i>0){
-            System.out.println("azaltma: "+baslangic);
-            return baslangic-=5*i;
-
+    // Recursive method to apply the specified rule
+    static int applyRule(int n) {
+        // Base case: If n is 0 or negative, return
+        if (n <= 0) {
+            System.out.println(n + " ");
+            return 1;
         }
-        if(i<0){
-            System.out.println("arttırma: "+baslangic);
-            return baslangic+=5*i;
 
-        }
-        return baslangic+desen(baslangic,baslangic/5-1);
+        // Print the current value
+        System.out.println(n + " ");
 
+        // Recursive call with n - 5
+        applyRule(n - 5);
 
+        // Print the current value again after the recursive call
+        System.out.println(n + " ");
+        return 1 ;
     }
-
 
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.println("sayı giriniz: ");
-        int sayi= input.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        desen(sayi,5);
-        System.out.println(desen(sayi,5));
+        // Get user input for N
+        System.out.print("Enter a value for N: ");
+        int n = scanner.nextInt();
+        System.out.println("girilen sayı: "+n);
+
+        // Apply the rule using the recursive method
+        applyRule(n);
+
     }
-
-
 }
