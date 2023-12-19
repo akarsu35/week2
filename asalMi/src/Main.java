1655793caa6f4daccce3fc403e6dcaf4d0ece337
akarsu35 <cumhurakarsu@gmail.com>
2023-12-19 21:36:19 +0300

    commit2

--- asalMi/src/Main.java
+++ asalMi/src/Main.java
@@ -20,12 +20,7 @@ public class Main {
         Scanner input=new Scanner(System.in);
         System.out.println("bir sayı giriniz: ");
         int sayi=input.nextInt();
-        isPrime(sayi,sayi-1);/* bolen değerini girilen sayıdan (sayi) bir önceki sayı ile başlatıp 1'e kadar gidiyoruz.
-                                  çünkü,girilen sayının(sayi) bütün bölenlerini kendisi ve kendisinden küçük sayılardan bazıları ouşturur.
-                                   ancak biz kendisine ve 1'e bölünüp bölünmediğini kontrol etmeyeceğiz.çünkü sayılar kendisine ve 1'e bölünür,
-                                   1'e kadar olan sayılardan böleni varsa [" if (num%bolen==0){return false;}" bloğuna girer] dolayısıyla sayı asal olmaz.
-                                   eğer 1'e kadar hiç bir bloğa girmezse doğal olarak "if(bolen==1){return true;}" bloğuna girecek.
-                                   yani sayının (sayi) 1'e kadar hiçbir böleni yoktur sayı asaldır.*/
+        isPrime(sayi,sayi-1);//sayının 1 eksiğinden 1'e kadar bölen arayan metod.
         if(isPrime(sayi,sayi-1)==false){
             System.out.println(sayi+ " sayısı asal değildir.");
         }else {
