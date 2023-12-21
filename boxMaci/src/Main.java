ecdefcff82bdd039314ef3fe57ef03da9ded3ad6
akarsu35 <cumhurakarsu@gmail.com>
2023-12-21 13:39:24 +0300

    commit1

--- boxMaci/src/Main.java
+++ boxMaci/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //Fighter sınıfından f1 nesnesi oluşturma
-        Fighter f1 = new Fighter("Marc" , 15 , 100, 90, 50);
+        Fighter f1 = new Fighter("Marc" , 15 , 100, 90, 40);
         //Fighter sınıfndan f2 nesnesi oluşturma
-        Fighter f2= new Fighter("Alex",15,100,95,25);
+        Fighter f2= new Fighter("Alex",15,100,95,45);
 
         Match match=new Match(f1,f2,80,100);//sporcuların denk olup olmadığını kontrol ediyoruz.
 
---
+++ harmonikDiziOrtalamasi/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+
+    public static double harmonik(int[] arr){//diziler için harmonik metodu tanımlıyoruz.
+        double sum=0.0;//başlnagıç toplamı
+        for(int i=0;i<arr.length;i++){
+            sum+=1.0/arr[i];//dizinin herbir elemanını harmonik yapıp topluyoruz.
+
+        }
+        return arr.length / sum;//dizinin eleman sayısını dizi ile oluşturulan
+                                // harmonik seri toplamına bölüyoruz.
+
+    }
+
+    public static void main(String[] args) {//herhangi bir dizi oluşturup metodu çağırıyoruz.
+        int[] numbers = {1,1,1,1};
+        System.out.println(harmonik(numbers));
+
+
+    }
+}
