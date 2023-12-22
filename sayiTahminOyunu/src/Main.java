307236524ed1de580975873ef27963d00bde6337
akarsu35 <cumhurakarsu@gmail.com>
2023-12-22 15:26:55 +0300

    commit1

---
+++ sayiTahminOyunu/src/Main.java
@@ -0,0 +1,69 @@
+
+import java.util.Random;//random sayı sınıfın dahil ediyoruz.
+import java.util.Scanner;//Scanner sınıfını dahil ediyoruz.
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        Random random=new Random();
+        String name;
+        int right=2;//deneme hakkı 3 olacak
+        int number;
+        boolean dongu=true;//while döngüsünün devamı için
+
+        int randomNumber= random.nextInt(100);
+        //System.out.println("random sayımız: "+randomNumber);// random sayı kontrolu için bu kodu açabilirsin
+
+        System.out.println("Adınızı giriniz: ");
+        name=input.nextLine();
+        System.out.println("0 ile 100 arasında bir sayı giriniz:");
+        number=input.nextInt();
+
+        while (dongu&&right!=0) {
+
+            if (number == randomNumber) {//girilen sayı random numaraya eşitse
+                System.out.println(name + " bildiniz, tebrikler!");
+                break;
+            }
+
+            else if (number < randomNumber) {//girilen sayı random numaradan küçükse
+                System.out.println("sayın " + name + " " + right + " hakkınız kaldı.");
+                System.out.println("büyük bir sayı giriniz: ");
+                number = input.nextInt();
+                right -= 1;
+                if (number == randomNumber) {//random sayıyı bilip bilmidiğinin kontrolu
+                    System.out.println(name + " bildiniz, tebrikler!");
+                    break;
+                }
+                if (right == 0) {//kalan hakkı sıfırsa
+                    System.out.println(name + " hakkınız bitti.");
+                    dongu = false;
+
+                }
+
+
+            } else if (number > randomNumber) {//girilen sayı random numaradan büyükse
+                System.out.println("sayın " + name + " " + right + " hakkınız kaldı.");
+                System.out.println("küçük bir sayı giriniz: ");
+                number = input.nextInt();
+                right -= 1;
+                if (number == randomNumber) {//random sayıyı bilip bilmidiğinin kontrolu
+                    System.out.println(name + " bildiniz, tebrikler!");
+                    break;
+                }
+                if (right == 0) {//kalan hakkı sıfırsa
+                    System.out.println(name + " hakkınız bitti.");
+                    dongu = false;
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
+
